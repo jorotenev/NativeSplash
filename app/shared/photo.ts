51beae583e25fdc8c7e242943e381db135ba1848
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-13 19:45:50 +0300

    Initial work on the unsplash api service

--- app/shared/photo.ts
+++ app/shared/photo.ts
@@ -1,7 +1,14 @@
+type url = string
+export type PhotoSize = "raw" | "full" | "regular" | "small" | "thumb"
+
 export interface Photo {
     id: string
-    url: string
     likes: number
+
     authorName: string
     authorHandle: string
+
+    // urls: { raw: string, full: string, regular: string, small: string, thumb: string }
+    urls: { [s in PhotoSize] : string}
+    download_link: url
 }
---
+++ app/shared/unsplash-service/api.ts
@@ -0,0 +1,69 @@
+import {HttpResponse} from "tns-core-modules/http";
+import * as http from "http";
+import {Photo, PhotoSize} from "~/shared/photo";
+
+const secrets = require("~/unsplash_secrets.json");
+
+const api_url = "https://api.unsplash.com/";
+const access_token = secrets.access_token; // you need an unsplash dev account for that.
+
+export function generatePhotoViewURL(photo: Photo, preferredWidth: number) {
+    let url = `${photo.urls.full}&w=${preferredWidth}`
+}
+
+/**
+ * https://unsplash.com/documentation#list-photos
+ * @param {UnsplashGetPhotosOpts} options
+ */
+export function getPhotos(options?: UnsplashGetPhotosOpts): Promise<Photo[]> {
+
+    const opts = {
+        per_page: 20,
+        page: 1,
+        ...options
+    };
+
+    const endpoint = "photos";
+    const url = `${api_url}${endpoint}/?page=${opts.page}&per_page=${opts.per_page}&client_id=${access_token}`;
+
+    return http.getJSON(url).then(function (response: UnsplashPhotosResult[]) {
+        console.dir(response);
+
+        // convert the response to our representation, since we don't need all of the properties.
+        return response.map(singlePhoto => {
+            return <Photo> {
+                id: singlePhoto.id,
+                created_at: singlePhoto.created_at,
+                authorHandle: singlePhoto.user.username,
+                authorName: singlePhoto.user.name,
+                likes: singlePhoto.likes,
+                urls: singlePhoto.urls,
+                download_link: singlePhoto.links.download
+            }
+        })
+    }, err => {
+        console.dir(err);
+        throw err
+    });
+}
+
+interface UnsplashGetPhotosOpts {
+    page: number,
+    per_page: number
+}
+
+
+// some of the properties that are received from the API for a single photo
+interface UnsplashPhotosResult {
+    id: string
+    created_at: string
+    updated_at: string
+    width: number
+    heigth: number
+    likes: number
+    description: string
+    user: { id: string, username: string, name: string }
+
+    urls: {[s in PhotoSize]:string} // hack
+    links: any // hack
+}
