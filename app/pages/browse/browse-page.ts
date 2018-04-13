8e304ca3dbd80dfe6c37020f7995090580363da1
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-13 23:06:51 +0300

    Initial version of the Browse page

--- app/pages/browse/browse-page.ts
+++ app/pages/browse/browse-page.ts
@@ -1,15 +1,43 @@
 import {NavigatedData, Page} from "tns-core-modules/ui/page";
 import {BrowseViewModel} from "~/pages/browse/browse-view-model";
+import {RadListView} from "nativescript-ui-listview";
+import {ItemEventData} from "tns-core-modules/ui/list-view";
 
 // declare the view-model
 let viewModel: BrowseViewModel;
 
+let listView: RadListView;
+
 export function onNavigatingTo(args: NavigatedData) {
+    console.log("onNavigatingTo");
     let page = <Page> args.object;
+
+    listView = page.getViewById('photos-list');
     viewModel = new BrowseViewModel();
+
     page.bindingContext = viewModel
+}
+
+
+export function onPullToRefreshInitiated(event) {
+    console.log("onPullToRefreshInitiated");
+    listView.notifyPullToRefreshFinished();
+}
+
+export function onTap(event: ItemEventData) {
+    console.log("tapped index" + event.index)
 }
 
 export function onLoadMoreItems(event) {
     console.log("[browse] loading more items");
+    // setTimeout due to https://github.com/NativeScript/NativeScript/issues/4931
+    setTimeout(() => {
+        viewModel.addMorePhotos()
+            .then(() => {
+                listView.notifyLoadOnDemandFinished()
+            }, () => {
+                listView.notifyLoadOnDemandFinished()
+            })
+    }, 50)
+
 }
--- app/pages/browse/browse-view-model.ts
+++ app/pages/browse/browse-view-model.ts
@@ -1,12 +1,53 @@
 import {Observable} from "tns-core-modules/data/observable";
 import {ObservableArray} from "tns-core-modules/data/observable-array";
 import {Photo} from "~/shared/photo";
+import {getPhotos} from "~/shared/unsplash-service/api"
 
 export class BrowseViewModel extends Observable {
+
+    // the array to which the UI is bind
     public photos: ObservableArray<Photo>;
+
+    // number of items which will be added to `photos` on each
+    // request for new photos. correspondents to unsplash's per_page
+    private readonly batchSize = 10;
 
     constructor() {
         super();
-        this.photos = new ObservableArray<Photo>([])
+        this.photos = new ObservableArray<Photo>([]);
+        this.addMorePhotos()
+    }
+
+    public addMorePhotos(): Promise<void> {
+        console.log("addMorePhotos called");
+        let currentlyLoadedPages = Math.ceil(this.photos.length / this.batchSize);
+
+        let photos = getPhotos({
+            page: currentlyLoadedPages + 1,
+            per_page: this.batchSize
+        })
+        // return Promise.resolve()
+        photos.then((newPhotos: Photo[]) => {
+            console.log(`${newPhotos.length} photos received by view-model`)
+            newPhotos.forEach(value => {
+                this.photos.push(value)
+            });
+        }, (err) => {
+            throw err
+        });
+        return new Promise<void>((resolve, reject) => {
+            // return Promise.resolve()
+            photos.then((newPhotos: Photo[]) => {
+                console.log(`${newPhotos.length} photos received by view-model`);
+
+                newPhotos.forEach(value => {
+                    this.photos.push(value)
+                });
+                resolve()
+            }, (err) => {
+                reject(err)
+            });
+
+        })
     }
 }
