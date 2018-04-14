97f7b228b40a8bd475c96f56896a5ddd887f3da9
Georgi Tenev <georgetenevbg@gmail.com>
2018-04-14 23:52:16 +0300

    add tab icons + schmidt! :scream:

--- app/pages/single/single-page.ts
+++ app/pages/single/single-page.ts
@@ -1,9 +1,6 @@
 import {NavigatedData, Page} from "tns-core-modules/ui/page";
 import {Photo} from "~/shared/photo";
-// var utilityModule = require("utils/utils");
-import * as utilityModule from "utils/utils";
-import {EventData, fromObject, Observable} from "tns-core-modules/data/observable";
-import {Button} from "tns-core-modules/ui/button";
+import {fromObject, Observable} from "tns-core-modules/data/observable";
 import {savePhoto} from "~/utils";
 import * as permissions from "nativescript-permissions";
 import * as dialogs from "ui/dialogs"
@@ -14,6 +11,7 @@ let bindingContext: Observable;
 export function onNavigatingTo(event: NavigatedData) {
     let page = <Page> event.object;
     photo = <Photo> event.context;
+
     bindingContext = fromObject({
         photo: photo,
         busy: false
@@ -22,9 +20,15 @@ export function onNavigatingTo(event: NavigatedData) {
     page.bindingContext = bindingContext
 }
 
+/**
+ * Figuring out how to save a photo was achieved with the help of:
+ * - https://github.com/NickIliev/NativeScript-Cosmos-Databank/blob/master/app/views/helpers/files/file-helpers.ts#L32
+ * - https://discourse.nativescript.org/t/how-do-i-create-a-folder-in-external-storage/3019/11
+ * - https://github.com/NickIliev/NativeScript-Cosmos-Databank/blob/d1db9e4a33a14ba558a2471089eec879589f35dd/app/views/drawer-page.js#L14-L26
+ */
 export function onSavePhoto() {
     console.log("save");
-    bindingContext.set('busy', true)
+    bindingContext.set('busy', true);
     permissions.requestPermission([
         "android.permission.READ_EXTERNAL_STORAGE",
         "android.permission.WRITE_EXTERNAL_STORAGE",
